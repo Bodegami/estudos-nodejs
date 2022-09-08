b9b7d794dd28109e7127e589b6c1cfb984d4cb3c
Renato <rena.f.oliveira@gmail.com>
2022-09-07 23:34:33 -0300

    Curso JS fundamentos - variaveis tipos numeros

---
+++ estudos-nodejs/javascript-fundamentos/tipo_number.js
@@ -0,0 +1,26 @@
+// TIPO NUMBER
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+// PONTO FLUTUANTE
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacaoMatematica = numeroPontoFlutuante / pontoFlutuanteSemZero;
+
+console.log(novaOperacaoMatematica);
+
+
+// NAN ----> Not A Number
+
+const alura = "alura";
+
+console.log(alura * meuNumero)
